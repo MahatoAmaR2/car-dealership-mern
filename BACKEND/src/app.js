6b29ea2223b4e86fb5a9f42ba1ac07a77cddfad3
MahatoAmaR2 <bauaa476@gmail.com>
2026-01-17 16:38:02 +0530

    Initial backend setup with Node.js and Express

---
+++ BACKEND/src/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Sample route
+app.get('/', (req, res) => {
+  res.send('Har Har Mahadev');
+});
+
+export {app}
